9b4a9dc82dfdccd0d0c8373e572e570407aa13ef
Grigoriy Lipilin <grigoriy.dp.ua48@gmail.com>
2024-08-26 15:27:41 +0100

    add homework 7

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+const input = document.querySelector('#name-input');
+const nameUser = document.querySelector('#name-output');
+const helloName = (event) => {
+    nameUser.textContent = event.currentTarget.value.trim();
+    if (!nameUser.textContent) {
+        nameUser.textContent = "Anonymous"; }
+}
+input.addEventListener('input', helloName);
